d1a99e05d75117d768c81f7b0949bfd8e3783b7d
Jian, Yu-An <anson3144@gmail.com>
2025-12-27 15:46:36 +0800

    Update animal.gd

--- scripts/animal.gd
+++ scripts/animal.gd
@@ -4,4 +4,4 @@ func _physics_process(delta: float) -> void:pass
 
 func _draw() -> void:
 	if OS.is_debug_build():
-		draw_rect(Rect2(),Color.CORNFLOWER_BLUE)
+		draw_rect(Rect2(Vector2(10,10),Vector2(100,100)),Color.CORNFLOWER_BLUE)
